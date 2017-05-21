68d7e0bd43e3c287e451c6462a590a08b9545350
andreby9527 <38912428@qq.com>
2017-05-21 20:50:41 +0800

    定义 下策略

--- LaserBoom-canal/src/main/java/xyz/spacexplore/support/CanalMessageListener.java
+++ LaserBoom-canal/src/main/java/xyz/spacexplore/support/CanalMessageListener.java
@@ -10,7 +10,8 @@ public class CanalMessageListener {
 	 */
 	@TransactionalEventListener(value = CanalMessageEvent.class, condition = "#canalMessageEvent.flag")
 	public void onApplicationEvent(CanalMessageEvent event) {
-		// mq 进行
+		//用枚举去做是不行的 枚举就是不可变常量池 缓存策略
+		// mq 进行  动态解析xml进行解析对应的类名及db或者collectionName 将配置文件的hash值 大小   或者 某个字段如version放置在redis或本地缓存或者内存中  如果
+		//如果 监听到 变化那么 将 static (redis中的原有读取数据进行重新存储 )判断的时候进行 拿全局就行
 	}
-
 }
